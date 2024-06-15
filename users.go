f38c20fdd98f49848ce72b3d81e10fe20470aa07
Arvind Suresh <arvind.suresh@smartstaff.co.in>
2024-06-15 23:10:41 +0530

    feat: [users] add rbac

--- users.go
+++ users.go
@@ -2,14 +2,23 @@ package main
 
 import "net/http"
 
+type Role struct {
+	Name        string
+	Permissions []string
+}
+
 type User struct {
 	Username string
 	Password string
+	Role     Role
 }
 
+var adminRole = Role{"admin", []string{"reserve_seat", "reset_seat", "view_seat"}}
+var userRole = Role{"user", []string{"reserve_seat", "view_seat"}}
+
 var users = []User{
-	{"admin", "password"},
-	{"superuser", "password"},
+	{"admin", "admin", adminRole},
+	{"user", "user", userRole},
 }
 
 func (u User) IsAuthorised(username, password string) bool {
